01f3484c8815a1b8247ca615d1d40ffab6e21cb9
Avi <avi27.999@gmail.com>
2019-10-08 23:35:53 +0530

    Updated code

--- OS/lab9/Bankers.c
+++ OS/lab9/Bankers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<string.h>
 int curr[5][5];
 int maxclaim[5][5];
 int avl[5];
@@ -7,7 +8,66 @@ int maxres[5];
 int running[5];
 int safe=0;
 int count = 0, i, j, exec, r, p, k = 1;
+int store[10];
+int k;
 
+
+void deadlock(){
+     k=0;
+     while (count != 0) {
+        safe = 0;
+        for (i = 0; i < p; i++) {
+            if (running[i]){
+                exec = 1;
+                for (j = 0; j < r; j++) {
+                    if (maxclaim[i][j] - curr[i][j] > avl[j]) {
+                        exec = 0;
+                        break;
+                    }
+                }
+                if (exec){
+                    printf("\nProcess%d is executing\n", i + 1);
+                    store[k++]=i+1;
+                    running[i] = 0;
+                    count--;
+                    safe = 1;
+
+                    for (j = 0; j < r; j++)
+                        avl[j] += curr[i][j];
+
+                    break;
+                }
+            }
+        }
+        if (!safe) {
+            printf("\n!!UNSAFE!!\n");
+            break;
+        } else {
+            printf("\nSAFE!");
+            printf("\nSafe sequence is:");
+
+            for (i = 0; i < k; i++) {
+                printf("\tP[%d]", store[i]);
+            }
+
+            printf("\n");
+        }
+    }
+}
+void allocated(){
+    memset(alloc,0,sizeof(int)*r);
+     for (i = 0; i < p; i++) {
+        for (j = 0; j < r; j++) {
+            alloc[j] += curr[i][j];
+        }
+    }
+}
+void available(){
+    memset(alloc,0,sizeof(int)*r);
+    for (i = 0; i < r; i++) {
+        avl[i] = maxres[i] - alloc[i];
+}
+}
 int main()
 {
     printf("\nEnter the number of processes: ");
@@ -62,21 +122,13 @@ int main()
 
         printf("\n");
     }
-
-    for (i = 0; i < p; i++) {
-        for (j = 0; j < r; j++) {
-            alloc[j] += curr[i][j];
-        }
-    }
+    allocated();
 
     printf("\nAllocated resources:");
     for (i = 0; i < r; i++) {
         printf("\t%d", alloc[i]);
     }
-
-    for (i = 0; i < r; i++) {
-        avl[i] = maxres[i] - alloc[i];
-    }
+    available();
 
     printf("\nAvailable resources:");
     for (i = 0; i < r; i++) {
@@ -84,7 +136,6 @@ int main()
     }
     printf("\n");
     
-    int store[10];
     int k=0;
     int req;
     int pno;
@@ -92,52 +143,31 @@ int main()
     printf("Do you want to enter another request?\n");
     puts("1(y)/0(n)");
     scanf("%d",&req);
-     if(req==1){
+     if(req==1)
+     {
         printf("Enter the process number\n");
         scanf("%d",&pno);
         for(int i=0;i<r;i++)
             scanf("%d",&newr[i]);
-    }
-
-    //if(req==1)
-    while (count != 0) {
-        safe = 0;
-        for (i = 0; i < p; i++) {
-            if (running[i]){
-                exec = 1;
-                for (j = 0; j < r; j++) {
-                    if (maxclaim[i][j] - curr[i][j] > avl[j]) {
-                        exec = 0;
-                        break;
-                    }
-                }
-                if (exec){
-                    printf("\nProcess%d is executing\n", i + 1);
-                    store[k++]=i+1;
-                    running[i] = 0;
-                    count--;
-                    safe = 1;
-
-                    for (j = 0; j < r; j++)
-                        avl[j] += curr[i][j];
-
-                    break;
-                }
+        for(int i=0;i<r;i++)
+            printf("%d\t",newr[i]);
+        for(int i=0;i<r;i++)
+            curr[pno][i]+=newr[i];
+        for(int i=0;i<p;i++){
+            for(int j=0;j<r;j++){
+                printf("%d\t",curr[i][j]);
             }
-        }
-        if (!safe) {
-            printf("\n!!UNSAFE!!\n");
-            break;
-        } else {
-            printf("\nSAFE!");
-            printf("\nSafe sequence is:");
-
-            for (i = 0; i < k; i++) {
-                printf("\tP[%d]", store[i]);
-            }
-
             printf("\n");
         }
-    }
 
+        allocated();
+        available();
+        for(int i=0;i<r;i++)
+            printf("%d\t",avl[i]);
+
+        deadlock();
+    }
+    else if(req==0)
+        deadlock();
+    return 0;   
 }
